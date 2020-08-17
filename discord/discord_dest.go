173d79be9abc45ec3f29d1cfbe2c51f1b09a2084
PLally <11771225+PLally@users.noreply.github.com>
2020-08-16 20:15:34 -0400

    add discordgo, add logrus, restructure project, posting to the send endpoint of an existing suggestion edits that suggestion

---
+++ discord/discord_dest.go
@@ -0,0 +1,100 @@
+package discord
+
+import (
+	"errors"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/cfc-servers/cfc_suggestions/suggestions"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+const editedAtTimeFormat = "Mon Jan _2 15:04:05 2006"
+
+type DiscordDestination struct {
+	session        *discordgo.Session
+	channelId      string
+	loggingChannel bool
+}
+
+func NewDest(channelId string, isLoggingChannel bool, session *discordgo.Session) *DiscordDestination {
+	return &DiscordDestination{
+		session:        session,
+		channelId:      channelId,
+		loggingChannel: isLoggingChannel,
+	}
+}
+
+func (dest *DiscordDestination) Send(suggestion *suggestions.Suggestion) (string, error) {
+	embed := dest.getEmbed(suggestion)
+	message, err := dest.session.ChannelMessageSendEmbed(dest.channelId, embed)
+
+	if err != nil {
+		err = fmt.Errorf("Error sending destination: %w", err)
+		log.Error(err)
+		return "", err
+	}
+
+	return message.ID, err
+}
+
+func (dest *DiscordDestination) SendEdit(suggestion *suggestions.Suggestion) (string, error) {
+
+	if suggestion.MessageID == "" {
+		return "", errors.New("Invalid message id")
+	}
+	embed := dest.getEmbed(suggestion)
+	embed.Footer = &discordgo.MessageEmbedFooter{
+		Text: fmt.Sprintf("Edited at %v", time.Now().Format(editedAtTimeFormat)),
+	}
+
+	message, err := dest.session.ChannelMessageEditEmbed(dest.channelId, suggestion.MessageID, embed)
+
+	if err != nil {
+		err = fmt.Errorf("Error sending destination: %w", err)
+		log.Error(err)
+	}
+
+	return message.ID, err
+}
+
+func (dest *DiscordDestination) getEmbed(suggestion *suggestions.Suggestion) *discordgo.MessageEmbed {
+	content := suggestion.Content
+	embed := &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf("%v Suggestion", content.Realm),
+		Description: fmt.Sprintf("**__%v__**", content.Title),
+		Color:       0x34EB77,
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   "Why?",
+				Value:  content.Why,
+				Inline: false,
+			},
+			{
+				Name:   "Why Not?",
+				Value:  content.WhyNot,
+				Inline: false,
+			},
+		},
+	}
+	if dest.loggingChannel || !content.Anonymous {
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:   "Author",
+			Value:  fmt.Sprintf("<@!%v>", suggestion.Owner),
+			Inline: false,
+		})
+	}
+	if dest.loggingChannel {
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:  "Is Edit",
+			Value: "True",
+		})
+		if suggestion.MessageID != "" {
+			embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+				Name:  "message id",
+				Value: suggestion.MessageID,
+			})
+		}
+	}
+	return embed
+}
---
+++ main_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/cfc-servers/cfc_suggestions/suggestions"
+	"github.com/cfc-servers/cfc_suggestions/suggestions/sqlite"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandlers(t *testing.T) {
+	server := suggestionsServer{
+		SuggestionStore: sqlite.NewStore("cfc_suggestion_test.db"),
+		suggestionsDest: &DummyDest{},
+		loggingDest:     &DummyDest{},
+		config: &suggestionsConfig{
+			Database: "cfc_suggestion_test.db",
+		},
+	}
+
+	newSuggestion, _ := server.Create("23701337384550")
+	r := mux.NewRouter()
+	r.HandleFunc("/suggestions", server.createSuggestionHandler).Methods(http.MethodPost, http.MethodOptions)
+	r.HandleFunc("/suggestions/{id}", server.getSuggestionHandler).Methods(http.MethodGet, http.MethodOptions)
+
+	testData := []struct {
+		handler        http.Handler
+		method         string
+		endpoint       string
+		body           string
+		expectedValues map[string]string
+		expectedStatus int
+		urlVars        map[string]string
+	}{
+		{
+			r,
+			"POST",
+			"/suggestions",
+			`{"owner": "179237013373845504"}`,
+			map[string]string{
+				"owner": "179237013373845504",
+			},
+			http.StatusCreated,
+			map[string]string{},
+		},
+		{
+			r,
+			"POST",
+			"/suggestions",
+			`{"owner": ""}`,
+			map[string]string{},
+			http.StatusBadRequest,
+			map[string]string{},
+		},
+		{
+			r,
+			"POST",
+			"/suggestions",
+			``,
+			map[string]string{},
+			http.StatusBadRequest,
+			map[string]string{},
+		},
+		{
+			r,
+			"GET",
+			fmt.Sprintf("/suggestions/%v", newSuggestion.Identifier),
+			``,
+			map[string]string{},
+			http.StatusOK,
+			map[string]string{"id": newSuggestion.Identifier},
+		},
+		{
+			r,
+			"GET",
+			fmt.Sprintf("/suggestions/%v", "13456"),
+			``,
+			map[string]string{},
+			http.StatusNotFound,
+			map[string]string{"id": "13456"},
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.endpoint, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			body := data.body
+			bodyReader := bytes.NewReader([]byte(body))
+			request := httptest.NewRequest(data.method, data.endpoint, bodyReader)
+			mux.SetURLVars(request, data.urlVars)
+			data.handler.ServeHTTP(recorder, request)
+
+			bodyBytes, _ := ioutil.ReadAll(recorder.Body)
+			var returnedValues map[string]string
+			json.Unmarshal(bodyBytes, &returnedValues)
+
+			if recorder.Code != data.expectedStatus {
+				t.Errorf("Incorrect status %v expected %v", recorder.Code, data.expectedStatus)
+			}
+			for k, v := range data.expectedValues {
+				responseValue, ok := returnedValues[k]
+				if !(ok && responseValue == v) {
+					t.Errorf("Incorect key %v", k)
+				}
+			}
+		})
+	}
+}
+
+type DummyDest struct {
+}
+
+func (*DummyDest) Send(*suggestions.Suggestion) (string, error)     { return "", nil }
+func (*DummyDest) SendEdit(*suggestions.Suggestion) (string, error) { return "", nil }
